da03cbd266e4d6f7aca731d966e8d3424f7fc5a9
AK <ak.ramadugu@gmail.com>
2020-12-03 00:01:58 -0500

    Added a TextEditor field for tech item descriptions and updated minimum project iOS to iOS14

--- Project Code/MyTech/View/AddListView.swift
+++ Project Code/MyTech/View/AddListView.swift
@@ -11,24 +11,50 @@ struct AddListView: View {
     @Environment(\.presentationMode) var presentationMode
     
     @State private var name: String = ""
-    @State private var techType: String = "Normal"
+    @State private var serialNumber: String = ""
+    @State private var productNumber: String = ""
+    @State private var modelNumber: String = ""
+    @State private var dateAcquired: Date = Date()
+    @State private var text = ""
+    @State private var techType: String = "Device"
     
     let techTypes = ["Device", "Component", "Accesory"]
+    
     // MARK: - BODY
     var body: some View {
         NavigationView{
             VStack {
                 Form {
-                    // MARK: - TECH NAME
-                    TextField("Tech", text: $name)
-                    
-                    // MARK: - TECH TYPE
-                    Picker("Tech Type", selection: $techType){
-                        ForEach(techTypes, id: \.self){
-                            Text($0)
+                    Section(header: Text("Details")){
+                        // MARK: - TECH NAME
+                        TextField("Tech Name", text: $name)
+                        
+                        // MARK: - TECH TYPE
+                        Picker("Tech Type", selection: $techType){
+                            ForEach(techTypes, id: \.self){
+                                Text($0)
+                            }
                         }
-                    }
-                    .pickerStyle(SegmentedPickerStyle())
+                        .pickerStyle(SegmentedPickerStyle())
+                        
+                        // MARK: - DATE ACQUIRED
+                        DatePicker("Date Acquired", selection: $dateAcquired, displayedComponents: .date)
+                        
+                        // MARK: - SERIAL NUMBER
+                        TextField("Serial Number", text: $serialNumber)
+                        
+                        // MARK: - PRODUCT NUMBER
+                        TextField("Product Number", text: $productNumber)
+                        
+                        // MARK: - MODEL NUMBER
+                        TextField("Model Number", text: $modelNumber)
+                    }//: Section 1
+                    
+                    Section(header: Text("ADDITIONAL INFORMATION")){
+                        // MARK: - DETAILS
+                        TextEditor(text: $text)
+                    }//: Section 2
+                    
                     // MARK: - SAVE BUTTON
                     Button(action: {
                         print("Save a new tech item")
@@ -40,11 +66,11 @@ struct AddListView: View {
             } //: VSTACK
             .navigationBarTitle("New Item", displayMode: .inline)
             .navigationBarItems(trailing:
-                                    Button(action: {
-                                        self.presentationMode.wrappedValue.dismiss()
-                                    } ){
-                                        Image(systemName: "xmark")
-                                    }
+                Button(action: {
+                    self.presentationMode.wrappedValue.dismiss()
+                                } ){
+                                    Image(systemName: "xmark")
+                                }
             )
         } //: NAVIGATION
     }
