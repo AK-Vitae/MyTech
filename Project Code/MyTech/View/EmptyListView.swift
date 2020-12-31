f260cb0906230a125a8744e4a01ce8207ee6e766
AK <ak.ramadugu@gmail.com>
2020-12-30 21:16:56 -0500

    Minor EmptyListView animation tweak

--- Project Code/MyTech/View/EmptyListView.swift
+++ Project Code/MyTech/View/EmptyListView.swift
@@ -27,7 +27,7 @@ struct EmptyListView: View {
             .padding(.horizontal)
             .opacity(isAnimated ? 1 : 0)
             .offset(y: isAnimated ? 0 : -50)
-            .animation(.easeOut(duration: 1.5))
+            .animation(.easeIn(duration: 1.5))
             .onAppear(perform: {
                 self.isAnimated.toggle()
             })
