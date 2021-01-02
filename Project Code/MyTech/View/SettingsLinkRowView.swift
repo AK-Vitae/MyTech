51d9d9784d0bd3524607d50952996f6fcb315d3c
AK <ak.ramadugu@gmail.com>
2021-01-02 16:32:30 -0500

    Created a custom SettingsRowView that will be used for opening external links

---
+++ Project Code/MyTech/View/SettingsLinkRowView.swift
@@ -0,0 +1,54 @@
+//
+//  SettingsLinkRowView.swift
+//  MyTech
+//
+//  Created by Akshith Ramadugu on 1/1/21.
+//
+
+import SwiftUI
+
+struct SettingsLinkRowView: View {
+    // MARK: - PROPERTIES
+    
+    var icon: String
+    var color: Color
+    var text: String
+    var link: String
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack{
+            ZStack {
+                RoundedRectangle(cornerRadius: 8, style: .continuous)
+                    .fill(color)
+                Image(systemName: icon)
+                    .imageScale(.large)
+                    .foregroundColor(Color.white)
+            } //: ZStack
+            .frame(width: 36, height: 36, alignment: .center)
+            
+            Text(text).foregroundColor(Color.gray)
+            Spacer()
+            Button(action: {
+                // MARK: - OPEN LINK
+                guard let url = URL(string: self.link), UIApplication.shared.canOpenURL(url) else {
+                    return
+                }
+                UIApplication.shared.open(url as URL)
+            }) {
+                Image(systemName: "chevron.right")
+                    .font(.system(size: 14, weight: .semibold, design: .rounded))
+            }
+            .accentColor(Color(.systemGray2))
+        }
+    }
+}
+
+// MARK: - BODY
+struct SettingsLinkRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLinkRowView(icon: "globe", color: Color.pink, text: "Source Code", link: "https://github.com/AK-Vitae/MyTech")
+            .previewLayout(.fixed(width: 375, height: 60))
+            .padding()
+    }
+}
